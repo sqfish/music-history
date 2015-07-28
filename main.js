444321d4e81983206c6f40c793b97e840459b272
Sally Fisher <sqfisher@gmail.com>
2015-07-28 12:30:56 -0500

    Placed song objects into separate JSON file and added corresponding AJAX to read in  and display info for each song

--- main.js
+++ main.js
@@ -1,22 +1,81 @@
+$(document).ready(function(){
 
-var songs = [];
+  $.ajax({
+    url: "songs.json"
+  }).done(function(data) {
+    var songArray = data.songs;
+
+    for (var i = 0; i < songArray.length; i++) {
+      var song = songArray[i];
+      var playlistHTML = document.getElementById("right-flex-item");
+      var childrenCount = playlistHTML.children.length;
+      if (i < childrenCount) {
+        updatePlaylist(song, i);
+      } else {
+        var newSong = document.createElement("DIV");
+        var newHeader = document.createElement("H3");
+        var newSongParagraph = document.createElement("P");
+        newSong.appendChild(newHeader);
+        newSong.appendChild(newSongParagraph);
+        playlistHTML.appendChild(newSong);
+        updatePlaylist(song, i);
+      }
+    }
+
+    var moreButton = document.createElement("BUTTON");
+    moreButton.id = "moreButton";
+    $(moreButton).attr("type", "button");
+    $(moreButton).text("More");
+    playlistHTML.appendChild(moreButton);
+
+
+    $("#moreButton").click(function(){
+      $.ajax({
+        url: "songs2.json"
+      }).done(function(result){
+        var songArray = result.songs;
+        for (var i = 0; i < songArray.length; i++) {
+          var song = songArray[i];
+          var playlistHTML = document.getElementById("right-flex-item");
+          var newSong = document.createElement("DIV");
+          var newHeader = document.createElement("H3");
+          var newSongParagraph = document.createElement("P");
+          newSong.appendChild(newHeader);
+          newSong.appendChild(newSongParagraph);
+          newSong = updateSongElement(song, newSong);
+          $(moreButton).before(newSong);
+        } 
+      })
+    })
+  })
+})
+
+
+
+
+// var moreButton = document.createElement("BUTTON");
+//     playlistHTML.appendChild(moreButton);
+
+
+
+// var songs = [];
 
 
 // Song object container
-function Song(title, artist, album) {
-  this.songName = title;
-  this.artistName = artist;
-  this.albumName = album;
-}
+// function Song(title, artist, album) {
+//   this.songName = title;
+//   this.artistName = artist;
+//   this.albumName = album;
+// }
 
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+// songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+// songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+// songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+// songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+// songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-songs.unshift("The Song That Never Ends > by Lamb Chop on the album Sing-Along, Play-Along");
-songs.push("Do Your Ears Hang Low? > by Unknown on the album Unknown");
+// songs.unshift("The Song That Never Ends > by Lamb Chop on the album Sing-Along, Play-Along");
+// songs.push("Do Your Ears Hang Low? > by Unknown on the album Unknown");
 
 
 
@@ -24,15 +83,15 @@ songs.push("Do Your Ears Hang Low? > by Unknown on the album Unknown");
 // Remove illegal characters and edit string
 // Update song item strings in array
 // 
-for (var i = 0; i < songs.length; i++) {
-  var currentValue = songs[i]; //get current song item
-  var illegalChar = /[*@(!]/g;
-  if (illegalChar.test(currentValue)) {
-    currentValue = currentValue.replace(illegalChar, "");
-  }
-  currentValue = currentValue.replace(/>/, "-");
-  songs[i] = currentValue;
-}
+// for (var i = 0; i < songs.length; i++) {
+//   var currentValue = songs[i]; //get current song item
+//   var illegalChar = /[*@(!]/g;
+//   if (illegalChar.test(currentValue)) {
+//     currentValue = currentValue.replace(illegalChar, "");
+//   }
+//   currentValue = currentValue.replace(/>/, "-");
+//   songs[i] = currentValue;
+// }
 
 
 
@@ -41,20 +100,20 @@ for (var i = 0; i < songs.length; i++) {
 // Create new song object and assign appropriate
 // name/value pairs.
 // 
-function parseSongInfo(currentValue) {
-  var query = " - ";
-  var x = currentValue.indexOf(query);
-  var songName = currentValue.slice(0, x);
-  query = "by ";
-  x = currentValue.indexOf(query);
-  x = x + query.length
-  query = " on the album ";
-  var y = currentValue.indexOf(query);
-  var artistName = currentValue.slice(x, y);
-  y = y + query.length;
-  var albumName = currentValue.slice(y);
-  return new Song(songName, artistName, albumName);
-}
+// function parseSongInfo(currentValue) {
+//   var query = " - ";
+//   var x = currentValue.indexOf(query);
+//   var songName = currentValue.slice(0, x);
+//   query = "by ";
+//   x = currentValue.indexOf(query);
+//   x = x + query.length
+//   query = " on the album ";
+//   var y = currentValue.indexOf(query);
+//   var artistName = currentValue.slice(x, y);
+//   y = y + query.length;
+//   var albumName = currentValue.slice(y);
+//   return new Song(songName, artistName, albumName);
+// }
 
 
 
@@ -63,14 +122,14 @@ function parseSongInfo(currentValue) {
 // Create new array containing to hold song objects
 // Add song objects with corresponding property/value pairs
 //
-var songArray = [];
-for (var i = 0; i < songs.length; i++) {
-  var currentValue = songs[i];
-  songArray[i] = parseSongInfo(currentValue);
-}
+// var songArray = [];
+// for (var i = 0; i < songs.length; i++) {
+//   var currentValue = songs[i];
+//   songArray[i] = parseSongInfo(currentValue);
+// }
 
-var playlistHTML = document.getElementById("right-flex-item");
-var songHTML = playlistHTML.firstElementChild;
+// var playlistHTML = document.getElementById("right-flex-item");
+// var songHTML = playlistHTML.firstElementChild;
 
 
 
@@ -109,19 +168,19 @@ function updatePlaylist(song, whichChild) {
 // Update existing playlist children song elements
 // Add new playlist children song elements if needed
 //
-for (var i = 0; i < songArray.length; i++) {
-  var song = songArray[i];
-  var playlistHTML = document.getElementById("right-flex-item");
-  var childrenCount = playlistHTML.children.length;
-  if (i < childrenCount) {
-    updatePlaylist(song, i);
-  } else {
-    var newSong = document.createElement("DIV");
-    var newHeader = document.createElement("H3");
-    var newSongParagraph = document.createElement("P");
-    newSong.appendChild(newHeader);
-    newSong.appendChild(newSongParagraph);
-    playlistHTML.appendChild(newSong);
-    updatePlaylist(song, i);
-  }
-}
+// for (var i = 0; i < songArray.length; i++) {
+//   var song = songArray[i];
+//   var playlistHTML = document.getElementById("right-flex-item");
+//   var childrenCount = playlistHTML.children.length;
+//   if (i < childrenCount) {
+//     updatePlaylist(song, i);
+//   } else {
+//     var newSong = document.createElement("DIV");
+//     var newHeader = document.createElement("H3");
+//     var newSongParagraph = document.createElement("P");
+//     newSong.appendChild(newHeader);
+//     newSong.appendChild(newSongParagraph);
+//     playlistHTML.appendChild(newSong);
+//     updatePlaylist(song, i);
+//   }
+// }
